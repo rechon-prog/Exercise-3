1910c7b70b32e4387bc375bec71472e4e73ab0e6
rechon-prog <rechon@paterostechnologicalcollege.edu.ph>
2026-03-12 00:10:06 +0800

    Add initial Python script with variable examples

---
+++ grp5_echon_ex3.py
@@ -0,0 +1,28 @@
+# My first simple script
+
+# Print introduction
+print("Hello, I'm Rian Anthony P. Echon! From 1V")
+
+# Python Variables
+number = "1V"
+name = "Rian"
+
+print(number)
+print(name)
+
+# Python - Variable Names (first names of group members, not including yours)
+ericka = "Ericka"
+mark = "Mark"
+jc = "Jc"
+micah = "Micah"
+nash = "Nash"
+
+print(ericka, mark, jc, micah, nash)
+
+# Many Values to Multiple Variables
+x, y, z = 1, 2, 3
+print(x, y, z)
+
+# One Value to Multiple Variables
+a = b = c = "Group1"
+print(a, b, c)
